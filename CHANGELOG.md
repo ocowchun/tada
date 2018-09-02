51e88b3c458c6f5ffcfa318dbea7c87eee55554d
ocowchun <ocowchun@gmail.com>
2018-09-02 21:41:04 +0800

    bump version to 0.0.3

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.0.3 (2018-09-02)
+* add `tada-github-review` to see review request
+* rename `tada-github` to `tada-github-pr`
+* refactor
--- tada.go
+++ tada.go
@@ -63,7 +63,7 @@ func testCommandFactory() (cli.Command, error) {
 }
 
 func main() {
-	c := cli.NewCLI("app", "0.0.2")
+	c := cli.NewCLI("app", "0.0.3")
 	c.Args = os.Args[1:]
 	c.Commands = map[string]cli.CommandFactory{
 		"run":     command.RunCommandFactory,
